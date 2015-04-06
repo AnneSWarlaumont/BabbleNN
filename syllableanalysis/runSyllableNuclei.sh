b48cbc2967b67bd7ace17b382ddb3e5827bc19f7
AnneSWarlaumont <anne.warlaumont@gmail.com>
2015-04-06 13:28:42 -0700

    Added code to analyze the synthesized sounds using de Jong & Wempe's Praat syllable nuclei detector.

---
+++ syllableanalysis/runSyllableNuclei.sh
@@ -0,0 +1,15 @@
+# rm /chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/SyllableData/NandScTesting_Firings_2_1_1_syllables.csv
+# /Applications/Praat.app/Contents/MacOS/Praat /Users/awarlau/IVOC-NN-Learning/BabbleNN/syllableanalysis/SyllableNucleiV2ASW.praat -25 2 0.3 "no" "/chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/Subset_Sims_Parameters_Used_In_Human_Trials/NandScTesting_Wave_2_1_1/" "/chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/SyllableData/NandScTesting_2_1_1_syllables.csv"
+
+# rm /chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/SyllableData/NandScTesting_Firings_2_1_2_syllables.csv
+# /Applications/Praat.app/Contents/MacOS/Praat /Users/awarlau/IVOC-NN-Learning/BabbleNN/syllableanalysis/SyllableNucleiV2ASW.praat -25 2 0.3 "no" "/chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/Subset_Sims_Parameters_Used_In_Human_Trials/NandScTesting_Wave_2_1_2/" "/chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/SyllableData/NandScTesting_2_1_2_syllables.csv"
+
+# rm /chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/SyllableData/NandScTesting_Firings_2_1_3_syllables.csv
+# /Applications/Praat.app/Contents/MacOS/Praat /Users/awarlau/IVOC-NN-Learning/BabbleNN/syllableanalysis/SyllableNucleiV2ASW.praat -25 2 0.3 "no" "/chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/Subset_Sims_Parameters_Used_In_Human_Trials/NandScTesting_Wave_2_1_3/" "/chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/SyllableData/NandScTesting_2_1_3_syllables.csv"
+
+# rm /chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/SyllableData/NandScTesting_Firings_2_1_4_syllables.csv
+# /Applications/Praat.app/Contents/MacOS/Praat /Users/awarlau/IVOC-NN-Learning/BabbleNN/syllableanalysis/SyllableNucleiV2ASW.praat -25 2 0.3 "no" "/chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/Subset_Sims_Parameters_Used_In_Human_Trials/NandScTesting_Wave_2_1_4/" "/chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/SyllableData/NandScTesting_2_1_4_syllables.csv"
+
+# rm /chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/SyllableData/NandScTesting_Firings_2_1_5_syllables.csv
+# /Applications/Praat.app/Contents/MacOS/Praat /Users/awarlau/IVOC-NN-Learning/BabbleNN/syllableanalysis/SyllableNucleiV2ASW.praat -25 2 0.3 "no" "/chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/Subset_Sims_Parameters_Used_In_Human_Trials/NandScTesting_Wave_2_1_5/" "/chroot/lab/BabbleNN/Neuron_and_MuscleScaling_Tests/SyllableData/NandScTesting_2_1_5_syllables.csv"
+
