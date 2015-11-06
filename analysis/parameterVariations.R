96336e1b5f793bf00c7d4e85d3bb78b957f8ef64
AnneSWarlaumont <anne.warlaumont@gmail.com>
2015-11-06 11:22:08 -0800

    Modified the script that analyzes the different parameter variations' results, to get information about variance across simulations and test for statistical significance of start vs. end values.

--- analysis/parameterVariations.R
+++ analysis/parameterVariations.R
@@ -4,12 +4,12 @@ variationkey = read.csv('parameterVariationsCSVFiles.csv')
 
 for (variation in 1:nrow(variationkey)){
 	
-	# Load the data fromt he 5 simulations of this type
+	# Load the data from the 5 simulations of this type
 	babblenndata = read.csv(as.character(variationkey$csvfile[variation]))
 	
 	# Get the first and last 60 seconds average salience for the 5 simulations of this type 50 motor neuron, muscle scaling = 4 simulations
 	babblennmin1 = subset(babblenndata,sec<=60&yoke=='false')
-	babblennmin120 = subset(babblenndata,sec>=(119*60)&sec<(120*60+1)&yoke=='false')
+	babblennmin120 = subset(babblenndata,sec>(119*60)&sec<=(120*60)&yoke=='false')
 	
 	print('~~~~~')
 	print(paste('Number of motor neurons: ',variationkey$nmot[variation]))
@@ -17,6 +17,10 @@ for (variation in 1:nrow(variationkey)){
 	print(paste('Minute 1 salience: ',mean(babblennmin1$salience)))
 	print(paste('Minute 120 salience: ',mean(babblennmin120$salience)))
 	print(paste('Minute 120 salience minus Minute 1 salience: ',mean(babblennmin120$salience)-mean(babblennmin1$salience)))
-	
-		
+	print(paste('Minute 1 standard deviation across simulations: ',sd(aggregate(babblennmin1$salience, by=list(babblennmin1$simname),FUN=mean)$x)))
+	print(paste('Minute 120 standard deviation across simulations: ',sd(aggregate(babblennmin120$salience, by=list(babblennmin120$simname),FUN=mean)$x)))
+	print(paste('Minute 1 standard error across simulations: ',sd(aggregate(babblennmin1$salience, by=list(babblennmin1$simname),FUN=mean)$x)/sqrt(5)))
+	print(paste('Minute 120 standard error across simulations: ',sd(aggregate(babblennmin120$salience, by=list(babblennmin120$simname),FUN=mean)$x)/sqrt(5)))
+	print(paste('T-test for difference in average salience between Minute 1 and Minute 120: '))
+	print(t.test(aggregate(babblennmin1$salience, by=list(babblennmin1$simname),FUN=mean)$x,aggregate(babblennmin120$salience, by=list(babblennmin120$simname),FUN=mean)$x,paired=TRUE))
 }
