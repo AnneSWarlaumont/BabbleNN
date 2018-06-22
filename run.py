4fe3af677214a12f92f9ebf8958079fe659e896c
Anne Sanda Warlaumont <awarlau@wifi-131-179-13-155.host.ucla.edu>
2018-06-22 13:33:00 -0700

    Adding script to run babble_daspnet_reservoir.py

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 12:42:32 2018
+
+@author: awarlau
+"""
+import sys
+sys.path.append('/Users/awarlau/Google Drive/Work/IVOC-NN-Learning/BabbleNN')
+import babble_daspnet_reservoir
+babble_daspnet_reservoir.sim('Mortimer','/Users/awarlau/Downloads',7200,'human',4,False,False)
